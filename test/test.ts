94ade89d47b4de2a9b3be0e5551633588c39f9bc
salak96 <lambangsasangka@gmail.com>
2023-09-21 15:23:08 +0700

    constructor function

--- test/test.ts
+++ test/test.ts
@@ -1,15 +1,23 @@
 describe ('Class', () => {
+    class Customer {
+        //constructor seperti function
+        constructor() {
+            console.info('Create new Customer');
+        }
+    }
+
+    class Order {
+    
+    }
     it('should can create class', () => {
-        class Customer {
-
-        }
-
-        class Order {
-        
-        }
-
+    
         const customer:Customer = new Customer();
         const order = new Order();
 
     });
+    it('should can create class', () => {
+        // Untuk memanggil object class
+        new Customer();
+        new Customer();
+    })
 });
