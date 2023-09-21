5b0df8ee30adeeb3259e9bc69a3792aa76d9f311
salak96 <lambangsasangka@gmail.com>
2023-09-21 16:30:42 +0700

    getter setter

--- test/properties.test.ts
+++ test/properties.test.ts
@@ -2,7 +2,7 @@ describe('Properties', () => {
     class Customer {
         // Property menggunakan titik dua
         readonly id: number; // Tidak bisa diubah (readOnly), wajib diisi!
-        name: string; // Type string, wajib diisi!
+        name: string; // Type string, wajib diisi! default value Guest
         age?: number; // Optional, type number
 
         // Constructor adalah method khusus yang dipanggil saat objek dibuat
@@ -11,14 +11,23 @@ describe('Properties', () => {
             this.id = id;
             this.name = name;
         }
+        //method
+        sayHello(name: string): void {
+            console.info(`Hello ${name}, my name is ${this.name}`);
+        }
     }
 
-    it('should be able to create an instance of the class', () => {
-        const customer: Customer = new Customer(1, 'Sasangka');
+    it('should be able to create an instance of the property', () => {
+        const customer: Customer = new Customer(1, 'Guest');
         customer.age = 29;
 
         console.info(customer.id);
         console.info(customer.name);
         console.info(customer);
+    });
+
+    it('should can have method', () => {
+        const customer = new Customer(1, 'Gawon');
+        customer.sayHello('Gawon');
     });
 });
