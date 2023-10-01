606498cb9e6a5ebeea0c7eaecbb823a6b1b64228
salak96 <lambangsasangka@gmail.com>
2023-10-01 11:33:13 +0700

    method overriding

--- test/inheritance.test.ts
+++ test/inheritance.test.ts
@@ -1,4 +1,4 @@
-describe('Inheritance', () => { 
+describe('Inheritance', () => {
     // Ini adalah deskripsi pengujian (test suite) dengan nama "Inheritance".
     // Parent class orangtua
     class Employee {
@@ -7,7 +7,7 @@ describe('Inheritance', () => {
 
         // Constructor dengan parameter 'name' bertipe string
         constructor(name: string) {
-            this.name = name; 
+            this.name = name;
             // Menginisialisasi properti 'name' dengan nilai yang diberikan pada parameter.
         }
     }
@@ -15,7 +15,7 @@ describe('Inheritance', () => {
     // Subclass Manager yang mewarisi dari Employee
     class Manager extends Employee {
         constructor(name: string) {
-            super(name); 
+            super(name);
             // Memanggil constructor dari superclass (Employee) dengan menggunakan 'super'.
         }
     }
@@ -23,25 +23,26 @@ describe('Inheritance', () => {
     // Subclass Director yang juga mewarisi dari Employee
     class Director extends Employee {
         constructor(name: string) {
-            super(name); 
+            super(name);
             // Memanggil constructor dari superclass (Employee) dengan menggunakan 'super'.
         }
     }
 
-    it('should be able to create an instance of the property', () => { // Ini adalah pengujian tunggal (test case).
-        const employee = new Employee('Eko'); 
+    it('should be able to create an instance of the property', () => {
+        // Ini adalah pengujian tunggal (test case).
+        const employee = new Employee('Eko');
         // Membuat instance baru dari kelas Employee dengan nama 'Eko'.
-        console.info(employee.name); 
+        console.info(employee.name);
         // Mencetak nama karyawan (employee) ke konsol.
 
-        const manager = new Manager('Budi'); 
+        const manager = new Manager('Budi');
         // Membuat instance baru dari kelas Manager dengan nama 'Budi'.
-        console.info(manager.name); 
+        console.info(manager.name);
         // Mencetak nama manajer (manager) ke konsol.
 
         const director = new Director('Tono');
-         // Membuat instance baru dari kelas Director dengan nama 'Tono'.
-        console.info(director.name); 
+        // Membuat instance baru dari kelas Director dengan nama 'Tono'.
+        console.info(director.name);
         // Mencetak nama direktur (director) ke konsol.
     });
 });
--- test/interface.test.ts
+++ test/interface.test.ts
@@ -2,14 +2,14 @@
 yang digunakan untuk mengelompokkan beberapa pengujian terkait dalam satu kelompok. 
 Dalam hal ini, kelompok ini akan menguji fitur-fitur terkait interface.
 */
-describe('interface',()=> {
+describe('interface', () => {
     /*
     interface HasName{ name : string; }
     Ini adalah deklarasi interface TypeScript yang disebut HasName. 
     Interface ini memiliki satu properti yaitu name dengan tipe data string.
     */
-    interface HasName{
-        name : string;
+    interface HasName {
+        name: string;
     }
     /*
     interface CanSayHello{ sayHello(name:string):void; }
@@ -17,8 +17,8 @@ describe('interface',()=> {
     Interface ini memiliki satu metode yaitu 
     sayHello yang menerima satu parameter berupa string dan mengembalikan void (tidak mengembalikan nilai).
     */
-    interface CanSayHello{
-        sayHello(name:string):void;
+    interface CanSayHello {
+        sayHello(name: string): void;
     }
     /*
     class Person implements HasName,CanSayHello{
@@ -28,35 +28,26 @@ describe('interface',()=> {
     Ini berarti kelas Person harus memiliki properti name dan 
     metode sayHello sesuai dengan definisi dari kedua interface tersebut.
     */
-    class Person implements HasName,CanSayHello{
+    class Person implements HasName, CanSayHello {
         name: string; ///Ini adalah properti dalam kelas Person yang sesuai dengan interface HasName. Properti ini akan menyimpan nama dari objek Person.
 
-        constructor(name:string){ //Ini adalah konstruktor dalam kelas Person. Ketika objek Person dibuat, konstruktor ini akan menerima parameter name dan menginisialisasi properti name dengan nilai yang diberikan.
+        constructor(name: string) {
+            //Ini adalah konstruktor dalam kelas Person. Ketika objek Person dibuat, konstruktor ini akan menerima parameter name dan menginisialisasi properti name dengan nilai yang diberikan.
             this.name = name;
         }
-        // Ini adalah implementasi metode sayHello yang sesuai dengan interface CanSayHello. 
+        // Ini adalah implementasi metode sayHello yang sesuai dengan interface CanSayHello.
         //Metode ini akan mencetak pesan salam dengan menggunakan nama yang diberikan sebagai parameter dan nama yang ada di dalam properti name dari objek Person.
-        sayHello(name:string):void {
-            console.info(`Hello ${name}, my name is ${this.name}`)
+        sayHello(name: string): void {
+            console.info(`Hello ${name}, my name is ${this.name}`);
         }
     }
     /* Ini adalah pengujian (test case) individual yang menggunakan it. 
     Pengujian ini bertujuan untuk memeriksa apakah pewarisan berfungsi dengan baik.
     */
-    it('should support inheritance',()=>{
-        const person =  new Person ("Sasangka")//Ini adalah pembuatan objek Person dengan nama "Sasangka".
-        person.sayHello("Lambang")
+    it('should support inheritance', () => {
+        const person = new Person('Sasangka'); //Ini adalah pembuatan objek Person dengan nama "Sasangka".
+        person.sayHello('Lambang');
         //Panggilan metode sayHello pada objek person dengan parameter "Lambang".
         //  Hasil panggilan ini akan dicetak dengan pesan salam yang menggabungkan nama "Lambang" dan nama yang ada di dalam objek person.
-    })
-})
-
-
-
-
-
-
-
-
-
-
+    });
+});
